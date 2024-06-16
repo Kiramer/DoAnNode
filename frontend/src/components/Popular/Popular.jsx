408461f98fc0ae8a1a1a453a74cac580dc9dd65d
spidey1309 <164634272+spidey1309@users.noreply.github.com>
2024-06-16 14:27:55 +0700

    update fr

--- frontend/src/components/Popular/Popular.jsx
+++ frontend/src/components/Popular/Popular.jsx
@@ -8,19 +8,22 @@ const Popular = ({ data }) => {
   const handleDetail = (id) => {
     navigate(`/productDetail/${id}`);
   };
+  const formatValue = (value) => {
+    return `${value?.toLocaleString("vi-VN")}`;
+  };
   return (
     <div className="popular">
       <h1>SẢN PHẨM BÁN NHIỀU NHẤT</h1>
       <hr />
       <div className="popular-item">
-        {data.slice(0, 6).map((item) => {
+        {data.slice(0, 5).map((item) => {
           return (
             <Item
               key={item._id}
               id={item._id}
               image={item.images}
               name={item.title}
-              price={item.price}
+              price={formatValue(item.price)}
               sold={item.sold}
               onClick={() => handleDetail(item._id)}
             />
